94a26fb89372b2be48a93792e77ee18dd6592802
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-17 23:04:27 +0530

    added: team mood component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,20 @@
 import Navbar from "./Navbar";
 import StatusCard from "./StatusCard";
+import TeamMoods from "./TeamMoods";
 
 const Layout = () => {
   return (
-    <div className="bg-[#EFEFEF] h-screen absolute w-full">
+    <div className="bg-[#EFEFEF] min-h-screen absolute w-full">
       <Navbar />
-      <div className="h-40 grid grid-cols-6 gap-8 mt-20 mx-auto w-11/12">
-        {[1, 2, 3, 4, 5].map((x) => (
-          <StatusCard x={x} />
-        ))}
+      <div className="h-screen grid grid-cols-6 grid-rows-3 gap-8 mt-20 mx-auto w-11/12">
+        <div className="col-span-5 grid h-40 grid-cols-5 gap-5 row-span-2">
+          {[1, 2, 3, 4, 5].map((x) => (
+            <StatusCard x={x} />
+          ))}
+        </div>
+        <div className="row-span-2 col-span-1">
+          <TeamMoods />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/TeamMoods.tsx
@@ -0,0 +1,11 @@
+const TeamMoods = () => {
+  return (
+    <div className="bg-white w-full h-full">
+      <div className="font-medium px-4 h-10 flex items-center">
+        Team mood
+      </div>
+    </div>
+  );
+};
+
+export default TeamMoods;
