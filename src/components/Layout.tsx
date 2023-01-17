2334f4e0cb7378abcf40d9bf89f6bc5460ffe785
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-17 20:55:23 +0530

    added: status icons

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
     <div className="bg-[#EFEFEF] h-screen absolute w-full">
       <Navbar />
       <div className="h-40 grid grid-cols-6 gap-8 mt-20 mx-auto w-11/12">
-        {[1, 2, 3, 4, 5, 6].map((x) => (
+        {[1, 2, 3, 4, 5].map((x) => (
           <StatusCard x={x} />
         ))}
       </div>
--- src/components/StatusCard.tsx
+++ src/components/StatusCard.tsx
@@ -1,4 +1,12 @@
-import { MdGridView } from "react-icons/md";
+import {
+  MdGridView,
+  MdOutlinePeople,
+} from "react-icons/md";
+import { BsCheck2Circle } from "react-icons/bs";
+import {
+  AiOutlineSync,
+  AiOutlineWarning,
+} from "react-icons/ai";
 
 const StatusCard = ({ x }: { x: number }) => {
   return (
@@ -8,7 +16,36 @@ const StatusCard = ({ x }: { x: number }) => {
         <p>Total Projects</p>
       </div>
       <span className="absolute left-4 top-4">
-        <MdGridView className="text-[#6DDDDC]" size={30} />
+        {x === 1 && (
+          <MdGridView
+            className="text-[#6DDDDC]"
+            size={30}
+          />
+        )}
+        {x === 2 && (
+          <BsCheck2Circle
+            className="text-[#6DDDDC]"
+            size={30}
+          />
+        )}
+        {x === 3 && (
+          <AiOutlineSync
+            className="text-[#6DDDDC]"
+            size={30}
+          />
+        )}
+        {x === 4 && (
+          <AiOutlineWarning
+            className="text-red-300"
+            size={30}
+          />
+        )}
+        {x === 5 && (
+          <MdOutlinePeople
+            className="text-[#6DDDDC]"
+            size={30}
+          />
+        )}
       </span>
     </div>
   );
