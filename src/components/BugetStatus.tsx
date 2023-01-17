7ba4ec43d0bd20e8313a957f7e33b4d69238c7e5
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-18 01:21:51 +0530

    style: updated btn styles

--- src/components/BugetStatus.tsx
+++ src/components/BugetStatus.tsx
@@ -10,19 +10,19 @@ const BudgetStatus = () => {
       <span className="font-medium text-xl">
         Budget status
       </span>
-      <span className="flex gap-5">
-        <button className="border bg-[#008075] text-white text-sm flex items-center border-gray-700 px-4 rounded-sm">
+      <span className="flex gap-3">
+        <button className="border bg-[#008075] text-white text-xs flex items-center border-gray-700 p-1 px-2 rounded-sm">
           <MdOutlineAdd size={15} /> Add New Project
         </button>
-        <button className="border border-gray-700 flex text-[#008075] text-sm px-4 items-center rounded-sm">
+        <button className="border border-gray-700 flex text-[#008075] text-xs p-1 px-2 items-center rounded-sm">
           <MdOutlineFileDownload size={15} /> Download
           report
         </button>
-        <button className="border flex border-gray-700 text-[#008075] px-4 items-center rounded-sm">
+        <button className="border flex border-gray-700 text-[#008075] text-xs p-1 px-2 items-center rounded-sm">
           <MdOutlineDateRange size={15} />{" "}
           dd/mm/yyyy-dd/mm/yy
         </button>
-        <button className="border flex items-center text-[#008075] border-gray-700 px-4 rounded-sm">
+        <button className="border flex items-center text-[#008075] border-gray-700 p-1 px-2 rounded-sm">
           <MdOutlineFilterAlt size={15} /> Filter
         </button>
       </span>
