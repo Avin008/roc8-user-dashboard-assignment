dfaa220b755c8d68d9cc79e579b16a83021686bf
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-20 11:29:50 +0530

    style: bg to delayed prop

--- src/components/StatusCard.tsx
+++ src/components/StatusCard.tsx
@@ -10,9 +10,17 @@ import {
 
 const StatusCard = ({ x }: { x: number }) => {
   return (
-    <div className="w-full relative bg-white h-full flex items-center justify-center shadow-lg">
-      <div className="flex flex-col gap-2 justify-center items-center">
-        <h2 className="font-black text-4xl">{x}</h2>
+    <div
+      className={`w-full relative bg-white h-full flex items-center justify-center shadow-lg ${
+        x === 4 && "bg-[#EB6482]"
+      }`}
+    >
+      <div
+        className={`flex flex-col gap-2 justify-center items-center ${
+          x === 4 && "text-white"
+        }`}
+      >
+        <h2 className="font-extrabold text-3xl">{x}</h2>
         <p>Total Projects</p>
       </div>
       <span className="absolute left-4 top-4">
