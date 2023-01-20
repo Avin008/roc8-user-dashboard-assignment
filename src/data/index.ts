cb22d3c7602a42ff4f0797241776800fd6639445
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-20 12:29:06 +0530

    added: updated project status data

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,9 +3,9 @@ export const projectStatus: {
   status: number;
   name: string;
 }[] = [
-  { id: 1, status: 1, name: "Total Projects" },
-  { id: 2, status: 2, name: "Completed" },
+  { id: 1, status: 5, name: "Total Projects" },
+  { id: 2, status: 1, name: "Completed" },
   { id: 3, status: 3, name: "Ongoing" },
-  { id: 4, status: 4, name: "Delayed" },
+  { id: 4, status: 1, name: "Delayed" },
   { id: 5, status: 5, name: "Employees" },
 ];
